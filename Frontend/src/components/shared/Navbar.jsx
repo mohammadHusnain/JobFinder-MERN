bccab87c60ab70b06769ee467ad4e5d7d12717a8
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-06-01 13:06:42 +0500

    added authentication and navbar

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -7,12 +7,13 @@ import { User2, LogOut } from "lucide-react";
 
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-6xl h-16">
         <div>
           <h1 className="text-2xl font-bold">
-            Job <span className="text-[#F83002]">Portal</span>
+            JOB <span className="text-blue-600">FINDER</span>
           </h1>
         </div>
 
@@ -23,43 +24,58 @@ const Navbar = () => {
             <li>Browse</li>
           </ul>
 
-          <Popover>
-            <PopoverTrigger asChild>
-<Avatar className="cursor-pointer">
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-      </Avatar >           
-       </PopoverTrigger>
-            <PopoverContent className="w-80">
+          {
+            !user ? (
 
-<div className="flex gap-4 space-y-2">
-<Avatar className="cursor-pointer">
-                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-
-              </Avatar>
-              <div>
-
-              <h4 className="font-medium">Muhammad Husnain</h4>
-              <p className="text-sm text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit iure, voluptatibus aliquid asperiores velit veniam accusantium quas incidunt eos dolores officiis. Ab ipsa nesciunt sequi illo, fugit sit impedit voluptate!</p>
+              <div className="flex items-center gap-2">
+                <Link to="/login">  <Button variant="outline">Login</Button>
+                </Link>
+                <Link to="/signup">  <Button className="bg-gray-800 text-white hover:bg-gray-900">Sign Up</Button>
+                </Link>
               </div>
-</div>
-<div className="flex flex-col my-2 text-gray-600">
 
-  <div className="flex w-fit items-center gap-2 cursor-pointer">
-    <User2 />
-    <Button variant="link">View Profile</Button>
-  </div>
+            ) : (
 
-  <div className="flex w-fit items-center gap-2 cursor-pointer">
-    <LogOut />
-    <Button variant="link">Logout</Button>
-  </div>
-  
-</div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  </Avatar >
+                </PopoverTrigger>
+                <PopoverContent className="w-80">
+
+                  <div className="flex gap-4 space-y-2">
+                    <Avatar className="cursor-pointer">
+                      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+
+                    </Avatar>
+                    <div>
+
+                      <h4 className="font-medium">Muhammad Husnain</h4>
+                      <p className="text-sm text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit iure, voluptatibus aliquid asperiores velit veniam accusantium quas incidunt eos dolores officiis. Ab ipsa nesciunt sequi illo, fugit sit impedit voluptate!</p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col my-2 text-gray-600">
+
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <User2 />
+                      <Button variant="link">View Profile</Button>
+                    </div>
+
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <LogOut />
+                      <Button variant="link">Logout</Button>
+                    </div>
+
+                  </div>
+
+                </PopoverContent>
+              </Popover>
+
+            )
+          }
 
 
-              
-            </PopoverContent>
-          </Popover>
         </div>
       </div>
     </div>
